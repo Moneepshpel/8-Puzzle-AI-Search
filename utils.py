9f55084cdf662dbedadd13a5be5ecdd460a7cc1b
Moneepshpel <moneepshpel7526@gmail.com>
2025-12-17 13:34:44 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,37 +1,89 @@
-def get_neighbors(state):
-    """
-    Return a list of neighbor states from the current state.
-    """
-    neighbors = []
-    zero_index = state.index(0)
-    swaps = []
-    # Left
-    if zero_index % 3 > 0:
-        swaps.append(-1)
-    # Right
-    if zero_index % 3 < 2:
-        swaps.append(1)
-    # Up
-    if zero_index // 3 > 0:
-        swaps.append(-3)
-    # Down
-    if zero_index // 3 < 2:
-        swaps.append(3)
+# utils.py
+# Utility functions and data structures used by all search algorithms
 
-    for s in swaps:
-        new_state = state[:]
-        new_state[zero_index], new_state[zero_index + s] = new_state[zero_index + s], new_state[zero_index]
-        neighbors.append(new_state)
+import heapq
 
-    return neighbors
+# -------------------------
+# Queue (for BFS)
+# -------------------------
+class Queue:
+    def __init__(self):
+        self.items = []
 
-def manhattan_distance(state, goal):
-    """Heuristic: sum of Manhattan distances of tiles from their goal positions."""
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop(0)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+
+# -------------------------
+# Stack (for DFS)
+# -------------------------
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+
+# -------------------------
+# Priority Queue (UCS, Greedy, A*)
+# -------------------------
+class PriorityQueue:
+    def __init__(self):
+        self.heap = []
+        self.count = 0   # to avoid comparison between nodes
+
+    def push(self, item, priority):
+        heapq.heappush(self.heap, (priority, self.count, item))
+        self.count += 1
+
+    def pop(self):
+        return heapq.heappop(self.heap)[2]
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
+
+# -------------------------
+# Heuristic Functions
+# -------------------------
+
+# Heuristic 1: Number of misplaced tiles
+def misplaced_tiles(state, goal_state):
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if state[i][j] != 0 and state[i][j] != goal_state[i][j]:
+                count += 1
+    return count
+
+
+# Heuristic 2: Manhattan Distance (Admissible)
+def manhattan_distance(state, goal_state):
     distance = 0
-    for num in range(1, 9):
-        idx_state = state.index(num)
-        idx_goal = goal.index(num)
-        x1, y1 = divmod(idx_state, 3)
-        x2, y2 = divmod(idx_goal, 3)
-        distance += abs(x1 - x2) + abs(y1 - y2)
+
+    goal_positions = {}
+    for i in range(3):
+        for j in range(3):
+            goal_positions[goal_state[i][j]] = (i, j)
+
+    for i in range(3):
+        for j in range(3):
+            value = state[i][j]
+            if value != 0:
+                goal_i, goal_j = goal_positions[value]
+                distance += abs(i - goal_i) + abs(j - goal_j)
+
     return distance
